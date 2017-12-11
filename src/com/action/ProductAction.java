c91eae547debf88bba720f95ed40a2f5ece3306f
moujianming <31647832+Moujianming@users.noreply.github.com>
2017-12-11 20:56:43 +0800

    获取response和request对象

--- src/com/action/ProductAction.java
+++ src/com/action/ProductAction.java
@@ -1,5 +1,10 @@
 package com.action;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts2.ServletActionContext;
+
 import com.bean.Product;
 
 public class ProductAction {
@@ -8,6 +13,11 @@ public class ProductAction {
 
 	public String show()
 	{
+		HttpServletRequest request = ServletActionContext.getRequest();
+        HttpServletResponse response = ServletActionContext.getResponse();
+ 
+        System.out.println("request:\t" + request);
+        System.out.println("response:\t" + response);
 		product = new Product();
 		product.setName("iphone");
 		return "show";
