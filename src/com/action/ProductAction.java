a87aae8668a171c54a7626bc67dfd02edd77b2dd
moujianming <31647832+Moujianming@users.noreply.github.com>
2017-12-11 19:53:29 +0800

    显示数据到jsp

---
+++ src/com/action/ProductAction.java
@@ -0,0 +1,25 @@
+package com.action;
+
+import com.bean.Product;
+
+public class ProductAction {
+
+	private Product product;
+
+	public String show()
+	{
+		product = new Product();
+		product.setName("iphone");
+		return "show";
+	}
+	public Product getProduct() {
+		System.out.println("getProduct method");
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		System.out.println("setProduct method");
+		this.product = product;
+	}
+	
+}
