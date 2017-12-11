278c294329073ce00b5f8b1eb8ff90307d1ab1be
moujianming <31647832+Moujianming@users.noreply.github.com>
2017-12-11 21:08:16 +0800

    session

--- WebContent/show.jsp
+++ WebContent/show.jsp
@@ -8,5 +8,6 @@
 </head>
 <body>
 ${product.name}
+${name}
 </body>
 </html>
--- src/com/action/ProductAction.java
+++ src/com/action/ProductAction.java
@@ -1,11 +1,14 @@
 package com.action;
 
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.struts2.ServletActionContext;
 
 import com.bean.Product;
+import com.opensymphony.xwork2.ActionContext;
 
 public class ProductAction {
 
@@ -19,11 +22,13 @@ public class ProductAction {
         System.out.println("request:\t" + request);
         System.out.println("response:\t" + response);
 		product = new Product();
-		product.setName("iphone");
+		product.setName("iphoneX");
 		return "show";
 	}
 	public String add()
 	{
+		Map m = ActionContext.getContext().getSession();
+		m.put("name", product.getName());
 		System.out.println("product.name:"+product.getName());
 		return "show";
 	}
